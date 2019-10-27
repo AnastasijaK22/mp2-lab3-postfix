2acc79b3f8914d41c511e1becd2277966967b39e
AnastasijaK22 <akuzenkova1999@gmail.com>
2019-10-27 16:51:09 +0300

    Реализовала методы стека и постфикса

---
+++ kuzenkova/base/postfix.cpp
@@ -0,0 +1,130 @@
+﻿#include "postfix.h"
+#include "stack.h"
+
+/*bool TPostfix::isCorrect(string str)
+{
+	string unacceptable = "!@#^&.,<>{}[]|=_1234567890?";
+	int count = 0, k = 0, countOperation = 0, length = str.length();
+	if ((operands.find(str[0]) != string::npos) || (operands.find(str[length - 1]) != string::npos))
+		return false;
+	for (int i = 0; i < length; i++)
+	{
+		if (unacceptable.find(str[i]) != string::npos)
+			return false;
+		if (str[i] == '(')
+			count++;
+		if (str[i] == ')')
+			count--;
+		if ((str[i] - 'a' >= 0) && (str[i] - 'a' <= 26))
+			k++;
+		else
+			k = 0;
+		if (operands.find(str[i]) != string::npos)
+			countOperation++;
+		else
+			countOperation = 0;
+		if ((k > 1) || (countOperation > 1))
+			return false;
+	}
+	if (count != 0)
+		return false;
+	return true;
+}*/
+
+//string TPostfix::ToPostfix()
+//{
+	//TStack<char> opStack(MaxStackSize);
+	//string tmp = "+-*/()";
+	/*for (int i = 0; i < infix.length(); i++)
+	{
+		if (tmp.find(infix[i]) == string::npos)
+			postfix += infix[i];
+		if (infix[i] == '(')
+			opStack.push(infix[i]);
+		if (infix[i] == ')')
+		{
+			char temp = opStack.pop();
+			while (temp != '(')
+			{
+				postfix += temp;
+				temp = opStack.pop();
+			}
+		}
+		if (operands.find(infix[i]) != string::npos)
+		{
+			if (opStack.empty())
+				opStack.push(infix[i]);
+			else
+			{
+				int index2 = operands.find(infix[i]);
+				while (!opStack.empty())
+				{
+					char temp = opStack.pop();
+					if (temp == '(')
+					{
+						opStack.push(temp);
+						break;
+					}
+					int index1 = operands.find(temp);
+					if (priority[index2] <= priority[index1])
+						postfix += temp;
+					else
+					{
+						opStack.push(temp);
+						break;
+					}
+				}
+				opStack.push(infix[i]);
+			}
+		}
+	}
+	while (!opStack.empty())
+	{
+		postfix += opStack.pop();
+	}
+	return postfix;
+}
+
+double TPostfix::Calculate()
+{
+    if (postfix.length() == 0) ToPostfix();
+	int length = postfix.length();
+	TStack<double> res(MaxStackSize);
+	string nameValues;
+	double *values = new double[length];
+	for (int i = 0; i < length; i++)
+	{
+		if (operands.find(postfix[i]) == string::npos)
+		{
+			double p;
+			if (nameValues.find(postfix[i]) == string::npos)
+			{
+				nameValues += postfix[i];
+				cout << "Введите значение переменной - " << postfix[i] << endl;
+				cin >> p;
+				values[nameValues.length() - 1] = p;
+			}
+			else
+			{
+				p = values[nameValues.find(postfix[i])];
+			}
+			res.push(p);
+		}
+		else
+		{
+			double val1, val2, val3;
+			val2 = res.pop();
+			val1 = res.pop();
+			switch (postfix[i])
+			{
+			case '+': val3 = val1 + val2; break;
+			case '-': val3 = val1 - val2; break;
+			case '*': val3 = val1 * val2; break;
+			case '/': val3 = val1 / val2; break;
+			}
+			res.push(val3);
+		}
+	}
+	delete[] values;
+	return res.pop();
+}*/
---
+++ kuzenkova/base/stack.h
@@ -0,0 +1,61 @@
+﻿#ifndef __STACK_H__
+#define __STACK_H__
+
+const int MaxStackSize = 100;
+
+template <class T>
+class TStack
+{
+  T *pMem;
+  int size;
+  int top;
+  TStack(const TStack &st){}
+public:
+  TStack(int _size)
+  {
+    size = _size;
+    top = -1;
+    if ((size < 1) || (size > MaxStackSize))
+      throw "Error";
+    pMem = new T[size];
+  }
+  bool empty()
+  {
+	  return top == -1;
+  }
+  bool full()
+  {
+	  return top == (size - 1);
+  }
+  void push(T val)
+  {
+	  if (full())
+		  throw "Error";
+	  pMem[++top] = val;
+  }
+  T pop()
+  {
+	  if (empty())
+		  throw "Error";
+	  return pMem[top--];
+  }
+  TStack& operator=(const TStack &st) 
+  {
+	  if (this != &st)
+	  {
+		  size = st.size;
+		  top = st.top;
+		  delete[] pMem;
+		  pMem = new T[size];
+		  for (int i = 0; i < top; i++)
+			  pMem[i] = st.pMem[i];
+	  }
+	  return *this;
+  }
+  ~TStack()
+  {
+    delete [] pMem;
+  }
+};
+
+#endif
